852895e7a1afe6c8c4ebd089f04e3aa6261163c4
luisaele <luisaele28@gmail.com>
2022-12-22 18:13:57 -0300

    subiendo nuevos cambios

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let tarjetasDestapadas = 0;
 let tarjeta1 = null;
 let tarjeta2 = null;
+let primerResultado = null;
+let segundoResultado = null; 
 
 let numeros = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
 numeros  = numeros.sort(()=>{return Math.random()-0.5});
@@ -12,7 +14,10 @@ function destapar(id){
 
 if(tarjetasDestapadas == 1){
   tarjeta1 = document.getElementById(id);
-  tarjeta1.innerHTML = 'Hola';
+  tarjeta1.innerHTML = primerResultado;
+  
+  tarjeta1.disabled = true; 
+
   }
 
 }
