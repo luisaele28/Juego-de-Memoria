ae0d45399e51c18909b19619505b98dfe559d3bc
luisaele <luisaele28@gmail.com>
2022-12-22 17:28:12 -0300

    subiendo cambios

---
+++ index.js
@@ -0,0 +1,18 @@
+let tarjetasDestapadas = 0;
+let tarjeta1 = null;
+let tarjeta2 = null;
+
+let numeros = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
+numeros  = numeros.sort(()=>{return Math.random()-0.5});
+console.log(numeros);
+
+function destapar(id){
+  tarjetasDestapadas++;
+  console.log(tarjetasDestapadas);
+
+if(tarjetasDestapadas == 1){
+  tarjeta1 = document.getElementById(id);
+  tarjeta1.innerHTML = 'Hola';
+  }
+
+}
